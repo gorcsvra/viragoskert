4f6fb1717ac7a1d2322cdb18bec70494c145ab75
gorcsvra <66862598+gorcsvra@users.noreply.github.com>
2022-04-19 13:36:29 +0200

    első commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,63 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container-fluid bg-image">
+  
+      <link rel="preconnect" href="https://fonts.gstatic.com" />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Fira+Code&display=swap"
+        rel="stylesheet"
+      />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Acme&display=swap"
+        rel="stylesheet"
+      />
+
+      <form method="post" @submit.prevent="szamol">
+        <div class="wrapper">
+          <div class="box box1">
+            <div class="description">
+              <h2>Adatok</h2>
+              <label for="kertSzelesség"> <p><strong>Kertszélesség: </strong></p></label>
+              <input type="number" id="kertSzelesseg" v-model="kertSzelesseg" /><span>m</span>
+              <label for="kertHosszusag"><p><strong> Kerthosszúság:</strong></p> </label>
+              <input type="number" id="kertHosszusag" v-model="kertHosszusag" /><span>m</span>
+              <button type="submit" class="glass2"><p><strong>Számol</strong></p> </button>
+            </div>
+          </div>
+
+          <div class="box box2">
+            <div class="description">
+              <h2>Eredmény</h2>
+              <p><strong>Tulipánok száma: </strong>{{ tulip }}<span>{{db}}</span></p>
+              <p><strong>Rózsák száma: </strong>{{ rozsa }}<span>{{db}}</span></p>
+            </div>
+          </div>
+        </div>
+      </form>
+  
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      kertSzelesseg: "",
+      kertHosszusag: "",
+      tulip: "",
+      rozsa: "",
+      db: "db"
+    };
+  },
+  methods: {
+    szamol() {
+      this.tulip =
+        (Math.floor(this.kertSzelesseg / 2 / 0.4) + 1) *
+        (Math.floor(this.kertHosszusag / 0.4) + 1);
+      this.rozsa =
+        (Math.floor(this.kertSzelesseg / 2 / 0.6) + 1) *
+        (Math.floor(this.kertHosszusag / 0.6) + 1);
+    },
+  },
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
